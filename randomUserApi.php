afc32badfa22d61c7dc6168a89d3669d915e0f03
MMoza <118842323+MMoza@users.noreply.github.com>
2023-05-15 18:44:52 +0200

    add users into bd
    add bd #4

--- randomUserApi.php
+++ randomUserApi.php
@@ -11,10 +11,38 @@ class RandomUserAPI {
         return json_decode($response, true);
     }
 }
-
 $api = new RandomUserAPI();
 $users = $api->getUsers();
 
 echo json_encode($users);
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "prueba_ajax";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+foreach ($users['results'] as $user) {
+    $name = $user['name']['first']." ".$user['name']['last'];
+    $email = $user['email'];
+    $phone = $user['phone'];
+    $city = $user['location']['city'];
+
+    $sql = "INSERT INTO usuario (name, email, phone, city)
+    VALUES ('$name', '$email', '$phone', '$city')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close(); 
 ?>
